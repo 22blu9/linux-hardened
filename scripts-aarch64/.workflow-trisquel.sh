577bd040d84c4f999ad234c374e56d42db2fa88d
22blu9 <clarets-pings9g@icloud.com>
2026-07-31 18:13:59 +0300

    Download and extract Trisquel ARM64 root filesystem

--- scripts-aarch64/.workflow-trisquel.sh
+++ scripts-aarch64/.workflow-trisquel.sh
@@ -1,3 +1,4 @@
+sudo su -
 wget https://github.com/raymondwong19/Trisquel-Ecne-12-Libre-Rootfs-ARM64-aarch64/releases/download/Ecne-rootfs-v1/trisquel-aarch64.tar.xz
 cd /
 tar -xzf trisquel-aarch64.tar.xz
