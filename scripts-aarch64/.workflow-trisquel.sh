3932c27b0e06787eb767f32af5baf7fc4b4ee479
22blu9 <clarets-pings9g@icloud.com>
2026-07-31 18:14:50 +0300

    Refactor script to use 'su' instead of 'sudo su'
    Removed 'sudo su -' and adjusted wget command location.

--- scripts-aarch64/.workflow-trisquel.sh
+++ scripts-aarch64/.workflow-trisquel.sh
@@ -1,5 +1,5 @@
-sudo su -
-wget https://github.com/raymondwong19/Trisquel-Ecne-12-Libre-Rootfs-ARM64-aarch64/releases/download/Ecne-rootfs-v1/trisquel-aarch64.tar.xz
+su -
 cd /
+wget https://github.com/raymondwong19/Trisquel-Ecne-12-Libre-Rootfs-ARM64-aarch64/releases/download/Ecne-rootfs-v1/trisquel-aarch64.tar.xz
 tar -xzf trisquel-aarch64.tar.xz
 cd
