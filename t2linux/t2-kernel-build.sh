4a1142173e7af964704514fc84b6bb8574e709f6
22blu9 <clarets-pings9g@icloud.com>
2026-07-31 12:23:14 +0300

    Enhance kernel build script with T2 patches
    Updated download message to include T2 patches and added patch application loop. I need @Provectus18 's review on this.

--- t2linux/t2-kernel-build.sh
+++ t2linux/t2-kernel-build.sh
@@ -14,10 +14,15 @@ rm -rf "$WORK_DIR"
 mkdir -p "$WORK_DIR"
 cd "$WORK_DIR"
 
-echo "[1/5] Downloading kernel ${KERNEL_VERSION}..."
+echo "[1/5] Downloading kernel ${KERNEL_VERSION} and T2 patches..."
+mkdir build && cd build
+git clone --depth=1 https://github.com/t2linux/linux-t2-patches patches
 wget "$DOWNLOAD_URL"
 tar -xf "linux-${KERNEL_VERSION}.tar.xz"
 cd "linux-${KERNEL_VERSION}"
+for patch in ../patches/*.patch; do
+    patch -Np1 < $patch
+done
 
 echo "[2/5] Configuring kernel..."
 make defconfig
@@ -28,6 +33,14 @@ scripts/config --disable DEBUG_INFO
 scripts/config --disable COMPILE_TEST
 scripts/config --enable CGROUPS
 scripts/config --enable NAMESPACES
+scripts/config --module CONFIG_BT_HCIBCM4377
+scripts/config --module CONFIG_HID_APPLETB_BL
+scripts/config --module CONFIG_HID_APPLETB_KBD
+scripts/config --module CONFIG_DRM_APPLETBDRM
+scripts/config --module CONFIG_T2BCE_CORE
+scripts/config --module CONFIG_T2BCE_VHCI
+scripts/config --module CONFIG_T2BCE_AUDIO
+scripts/config --module CONFIG_APFS_FS
 
 make olddefconfig
 
